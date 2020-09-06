fb3b421984a6dfc7b7f6546aea80d2ed3a08ad7c
ankur <kushwahaankur30@gmail.com>
2020-09-06 23:34:29 +0200

    requirement files

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class MyTest(TestCase):
+
+    def normal_test(self):
+        self.assertEqual("Ankur", "Ankur")
+        print("ankur")
