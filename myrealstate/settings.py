fabb7110868f039a15c62fc8e4956f32f1b92a61
ankur <kushwahaankur30@gmail.com>
2020-09-07 00:13:43 +0200

    remove email

--- myrealstate/settings.py
+++ myrealstate/settings.py
@@ -148,11 +148,7 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'teamankur30@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ankur@01'
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
